30ada653fe5316bb05057906c16599559795e694
Hayato <olijnikm132@gamail.com>
2024-10-20 15:26:16 +0300

    Added new types of words

--- C++/WorldGame.cpp
+++ C++/WorldGame.cpp
@@ -5,14 +5,13 @@
 
 using namespace std;
 
-string RandomArr(string words[]){
-    int i = rand() % 199; 
-    return words[i];
+string RandomArr(string *words[]){
+    return words[rand() %3][rand()%199];
 }
 
-void Game(string words[], int score, auto start_time) { 
+void Game(string *wordArrays[], int score, auto start_time) { 
     string user;
-    string word = RandomArr(words);
+    string word = RandomArr(wordArrays);
     cout << "Repeat this word: " << word << endl;
     cin >> user;
     cout << endl;
@@ -33,14 +32,37 @@ void Game(string words[], int score, auto start_time) {
         cout << "You win in: " << elapsed_sec.count() << " seconds!" << endl; 
     }
     else{
-        Game(words,score,start_time);
+        Game(wordArrays,score,start_time);
     }
 }
 
 int main() {
     int score = 0; 
     srand(time(NULL));
-    string words[200] = {
+    string words_light[200] = {
+    "Ant", "Bat", "Cup", "Dog", "Ear", "Fish", "Gate", "Hat", "Ice", "Jam", 
+    "Key", "Leg", "Map", "Net", "Owl", "Pen", "Quill", "Rat", "Sun", "Top", 
+    "Up", "Van", "Web", "Xray", "Yarn", "Zoo", "Apple", "Book", "Car", 
+    "Doll", "Egg", "Fan", "Goat", "Hen", "Ink", "Jet", "Kite", "Leaf", 
+    "Mud", "Nail", "Orange", "Pup", "Quiz", "Ring", "Sock", "Toy", "Umbrella", 
+    "Violin", "Water", "Xylophone", "Yard", "Zip", "Antler", "Bear", "Cat", "Duck", 
+    "Eggplant", "Flag", "Goose", "Hat", "Iron", "Jelly", "Kangaroo", "Lamp", "Mug", 
+    "Nose", "Octopus", "Pig", "Queen", "Rose", "Stone", "Truck", "Upward", 
+    "Vine", "Window", "Yoyo", "Zebra", "Alligator", "Bus", "Chair", "Desk", 
+    "Elephant", "Fan", "Glass", "Hut", "Inch", "Joker", "Knot", "Lemon", "Milk", 
+    "Nest", "Oven", "Pencil", "Quiet", "Road", "Soup", "Table", "Umbrella", 
+    "Vase", "Wheel", "Xray", "Yolk", "Zipper", "Apple", "Bread", "Cup", "Dog", 
+    "Egg", "Fire", "Grape", "Hat", "Ice", "Jacket", "Key", "Lime", "Mouse", 
+    "Nut", "Orchard", "Plane", "Queen", "Ring", "Shirt", "Tree", "Umbrella", 
+    "Violet", "Watch", "Xray", "Yawn", "Zebra", "Ax", "Bat", "Cake", "Drum", 
+    "Ear", "Fish", "Goat", "Hat", "Ink", "Jug", "Kite", "Leaf", "Moon", 
+    "Nut", "Olive", "Pot", "Quiz", "Rat", "Sun", "Tap", "Umbrella", "Vase", 
+    "Window", "Xray", "Yellow", "Zoo", "Arrow", "Bread", "Cup", "Dog", 
+    "Egg", "Fan", "Grape", "Hat", "Ink", "Jug", "Key", "Leaf", "Map", 
+    "Net", "Orange", "Pen", "Queen", "Rat", "Sock", "Top", "Umbrella", 
+    "Vase", "Water", "Xylophone", "Yarn", "Zebra"
+};
+    string words_medium[200] = {
         "Apple", "Ball", "Cat", "Dog", "Eagle", "Forest", "Grape", "House", "Igloo", "Jungle",
         "Kite", "Lion", "Mouse", "Noble", "Ocean", "Piano", "Queen", "River", "Sun", "Tree",
         "Unicorn", "Vase", "Wolf", "Xylophone", "Yellow", "Zebra", "Artist", "Brave", "Circle", 
@@ -62,11 +84,36 @@ int main() {
         "Jewel", "Keen", "Lush", "Mirth", "Nest", "Orchid", "Pulse", "Quake", "Rope", 
         "Spire", "Tome", "Unity", "Vault", "Wisp", "Xenon", "Yield", "Zone"
     };
+    string words_hard[200] = {
+    "Algorithm", "Binary", "Cipher", "Dystopia", "Entropy", "Fractal", "Genome", 
+    "Heuristic", "Isotope", "Jargon", "Kinetics", "Lagrange", "Matrix", "Neutron", 
+    "Oscillation", "Paradox", "Quantum", "Relativity", "Singularity", "Topology", 
+    "Uncertainty", "Vector", "Wavelength", "Xenobiotic", "Yield", "Zygote", "Anomaly", 
+    "Blockchain", "Catalyst", "Dyson", "Enigma", "Fission", "Graviton", "Hypothesis", 
+    "Inertia", "Joules", "Krypton", "Lattice", "Molecule", "Neoplasm", "Oxidation", 
+    "Photon", "Quasar", "Resonance", "Schrodinger", "Thermodynamics", "Ultrasonic", 
+    "Voltage", "Wormhole", "Xylem", "Yottabyte", "Zero", "Amplitude", "Brane", "Chaotic", 
+    "Derivative", "Ecliptic", "Fusion", "Gluon", "Harmonics", "Invariance", "Joule", 
+    "Kelvin", "Lemmas", "Meson", "Nucleus", "Oscillator", "Polarization", "Quorum", 
+    "Relic", "Superposition", "Transistor", "Ubiquitous", "Vortex", "Wavelength", 
+    "Xenomorph", "Y-axis", "Zephyr", "Aperture", "Boson", "Curvature", "Darkmatter", 
+    "Epidemic", "Flux", "G-force", "Hyperbolic", "Inductor", "Jumpgate", "Kinematic", 
+    "Lorentz", "Magnetar", "Nanobot", "Omicron", "Panspermia", "Quotient", "Radioactive", 
+    "Singularity", "Tachyon", "Uranium", "Virtual", "Wavefunction", "Xerox", "Yield", 
+    "Zeppelin", "Angular", "Boltzmann", "Critical", "Dipole", "Exoplanet", "Fermion", 
+    "Gauss", "Heliosphere", "Interstellar", "Jetstream", "Klein", "Logarithm", "Mandelbrot", 
+    "Nonlinear", "Orbit", "Plasma", "Quanta", "Resistor", "String", "Tensor", "Ultraviolet", 
+    "Vacuum", "Wavelength", "X-axis", "Yttrium", "Zeno", "Asteroid", "Bitrate", "Cyclotron", 
+    "Diode", "Enthalpy", "Fourier", "Gigahertz", "Higgs", "Infrared", "Jacobian", 
+    "Kinematics", "Lorentzian", "Mass", "Nanotube", "Oblate", "Positron", "Quantum", 
+    "Radiation", "Schwarzschild", "Turing", "Ultrasound", "Volt", "Wave", "Xenon", 
+    "Ytterbium", "Zeptosecond"
+};
     SetConsoleCP(65001); 
     SetConsoleOutputCP(65001); 
     cout << "Hello, it's Word Game!" << endl;
     cout << "You need to repeat the word to win!" << endl;
     auto start_time = chrono::steady_clock::now();
-    Game(words, score, start_time);
-    return 0;
+    string *wordArrays[] = {words_light, words_medium, words_hard};
+    Game(wordArrays, score, start_time);
 }
